37f747ce7b4a24ef479108016587ed3455d0e233
henriquefernandez <hriqueft@gmail.com>
2018-02-07 13:20:54 -0200

    More of functions

--- modulos_e_funcoes.ex
+++ modulos_e_funcoes.ex
@@ -48,7 +48,22 @@ defmodule ModulosEFuncoes do
         Enum.map([1,2,3,4], &(&1 + &2))
     end
 
-    # Podemos também definr funções de uma forma simplificada, em apenas uma linha
+    # Referenciando funções pela ariedade
+    def reference_function_by_arity() do
+        
+        [1,2,3,4]
+        |> Enum.map(&more_than_one/1) # Como temos duas funções com o mesmo nome mais ariedade diferentes, temos que especificar qual é o more_than_one que queremos referenciar
+    end
+
+    def more_than_one(x) do
+        x + 1
+    end
+
+    def more_than_one(x, y) do
+        x + y
+    end
+
+    # Podemos também defnir funções de uma forma simplificada, em apenas uma linha
     def simplificada(x), do: x + @attribute_int
 
     # Podemos definir passar argumentos para uma função através dos ().
