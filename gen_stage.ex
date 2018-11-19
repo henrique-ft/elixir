17312f590b3e5090a119ae1a9b72fb73832b23c1
henriquefernandez <hriqueft@gmail.com>
2018-11-19 18:05:31 -0200

    update

---
+++ gen_stage.ex
@@ -0,0 +1,9 @@
+defmodule GenStage do
+  # Metodologia dentro da própria linguagem
+  # Cria processos na linguagem com memória compartilhada
+  # -----------------------------------------------------
+  # Producer: criar informação, 
+  # Consumer: consumir informação, 
+  # ProducerConsumer: produz e consome informação
+  # -----------------------------------------------------
+end
